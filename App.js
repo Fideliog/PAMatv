9ab6e341d1080a321fcc66800ea6520c113faf60
EDUARDO SANTOS LÊDO <eduardo.ledo@etec.sp.gov.br>
2025-08-08 12:20:15 -0300

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import casual_dog from './assets/casual_dog.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={casual_dog} />
+
+      <Text style={styles.title}>Ótimo dia!</Text>
+      <Text style={styles.subtitle}>Como deseja acessar?</Text>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Como deseja acessar?</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Outras opções</Text>
+      </TouchableOpacity>
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +30,33 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#43d38d',
+    paddingVertical: 14,
+    paddingHorizontal: 32,
+    borderRadius: 8,
+    borderWidth: 1
+  },
+  button2: {
+    paddingVertical: 14,
+    paddingHorizontal: 32,
+    borderRadius: 8,
+    borderWidth: 1
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#fff',
   },
 });
