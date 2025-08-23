df457495fa51e90a2ff7457a4028e5cd3b60edc4
Fideliog <eduardoledo36@gmail.com>
2025-08-23 20:20:21 -0300

    Acessar terminado

--- screens/Login.js
+++ screens/Login.js
@@ -1,10 +1,10 @@
-import { View, Text, Button, StyleSheet, TextInput, Alert } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { View, Text, Button, StyleSheet, TextInput, Alert, TouchableOpacity } from 'react-native';
 import { useState } from "react";
  
 export default function Login({ navigation }) {
     const [usuario, setEmail] = useState(''); 
     const [senha, setSenha] = useState('');
+    const [isSelected, setSelection] = useState(false);
     const usuarioValido = 'email'
     const senhaValida = 'senha'
 
@@ -14,26 +14,38 @@ export default function Login({ navigation }) {
         } else {
         Alert.alert('Erro', 'Usuário ou senha inválidos');
         }
-        
-    const toggleSecureText = () => {
-        setSecureText(!secureText);
-        };
-
     }
     return (
         <View style={styles.container}>
+
             <Text style={styles.title}>Acesse</Text>
             <Text style={styles.textoExtra}>Com email e senha</Text>
 
-            <View style={styles.buttonContainer}>
-                <Text style={styles.textInput}>E-mail</Text>
-                <TextInput style={styles.Input} placeholder="Digite seu E-mail" value={usuario} onChangeText={setEmail}/> 
-                <Text style={styles.textInput}>Senha</Text>
-                <TextInput style={styles.Input} placeholder="Digite sua senha" value={senha} onChangeText={setSenha}/>
-                <Button
-                    title="Logar"
-                    onPress={() => ValidarLogin()}
-                />
+            <Text style={{fontSize: 15}}>E-mail</Text>
+            <TextInput style={styles.Input} placeholder="Digite seu E-mail" value={usuario} onChangeText={setEmail}/> 
+
+            <Text style={{fontSize: 15}}>Senha</Text>
+            <TextInput style={styles.Input} placeholder="Digite sua senha" value={senha} onChangeText={setSenha}/>
+
+            <View style={styles.subInputs}>
+                <TouchableOpacity onPress={() => setSelection(!isSelected)} style={{flexDirection: 'row'}}>
+                    <View style={[styles.checkbox, isSelected && {backgroundColor: '#62daa0'}]} />
+                    <Text style={{ marginLeft: 8 }}>Lembrar senha</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <Text>Esqueci minha senha</Text>
+                </TouchableOpacity>
+            </View>
+
+            <View style={styles.subInputs}>
+                  <TouchableOpacity style={styles.button} onPress={() => ValidarLogin()}>
+                    <Text style={styles.buttonText}>Acessar</Text>
+                  </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.button2}>
+                        <Text style={styles.buttonText2}>Cadastrar</Text>
+                    </TouchableOpacity>
             </View>
         </View>
     );
@@ -45,21 +57,17 @@ const styles = StyleSheet.create({
         height: 'auto',
         marginLeft: '3%',
     },
+
     title: {
         fontSize: 50,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
     },
-    buttonContainer: {
-        margin: 10,
-        borderRadius: 5,
-    },
+
     textoExtra: {
         fontSize: 18,
         marginBottom: '5%',
     },
-    textInput: {
-        fontSize: 15,
-    },
+
     Input: {
         backgroundColor: '#eaeced',
         placeholderTextColor: '#bec3c7',
@@ -68,5 +76,55 @@ const styles = StyleSheet.create({
         paddingHorizontal: '3%',
         borderRadius: 8,
         marginBottom: 15,
+    },
+
+    checkbox: {
+        width: 20,
+        height: 20,
+        borderWidth: 1,
+        borderColor: '#43d38d',
+        borderRadius: 4,
+        backgroundColor: '#fff',
+    },
+
+    subInputs: {
+        alignContent: 'center', 
+        flexDirection: 'row',
+        width: '90%',
+        height: '15%',
+        justifyContent: 'space-between',
+    },
+
+    button: {
+        backgroundColor: '#43d38d',
+        paddingVertical: '2%',
+        borderRadius: 8,
+        borderWidth: 1,
+        borderColor: '#43d38d',
+        width: '47%',
+        height: 'auto',
+        justifyContent:'center',
+    },
+
+    button2: {
+        paddingVertical: '2%',
+        borderRadius: 8,
+        borderWidth: 1,
+        borderColor: '#43d38d',
+        width: '47%',
+        height: 'auto',
+        justifyContent:'center',
+    },
+
+    buttonText: {
+        fontSize: 15,
+        color: '#fff',
+        textAlign: 'center'
+    },
+
+    buttonText2: {
+        fontSize: 15,
+        color: '#black',
+        textAlign: 'center'
     },
 });
--- screens/PreLogin.js
+++ screens/PreLogin.js
@@ -6,14 +6,16 @@ import Google from '../assets/Google.png';
 export default function PreLogin({ navigation }) {
   return (
     <View style={styles.container}>
-      <Image style={styles.dog} resizeMode="contain" source={casual_dog}/>
+      <Image style={{width: '90%'}} resizeMode="contain" source={casual_dog}/>
 
       <Text style={styles.title}>Ótimo dia!</Text>
       <Text style={styles.subtitle}>Como deseja acessar?</Text>
 
       <TouchableOpacity  style={styles.button} onPress={() => navigation.navigate('Login')}>
         <View style={styles.google}><Image source={Google} style={{ width: 30, height: 30}}/></View>
-        <Text style={styles.buttonText}> Acessar com email </Text>
+        <View style={{  flex: 1, alignItems: 'center'}}>
+          <Text style={styles.buttonText}> Acessar com email </Text>
+        </View>
       </TouchableOpacity>
 
       <TouchableOpacity style={styles.button2}>
@@ -50,25 +52,21 @@ const styles = StyleSheet.create({
     borderRadius: 8, 
     borderWidth: 2, 
     borderColor: 'white',
-  },
-
-  dog: {
-    width: '90%',
+    alignSelf: 'flex-start',
   },
   
   button: {
-    alignContent: 'center',
     backgroundColor: '#43d38d',
     paddingVertical: '2%',
     paddingHorizontal: '3%',
     borderRadius: 8,
     borderWidth: 1,
     borderColor: '#43d38d',
-    justifyContent: 'center',
     alignItems:'center',
     flexDirection: 'row',
     marginBottom: 15,
     width:'90%',
+    position: 'relative',
     },
 
   button2: {
